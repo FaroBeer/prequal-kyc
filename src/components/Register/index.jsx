49716ee0e1cf46cc254321ddd631727475ecab92
FaroBeer <calmapazienza@gmail.com>
2018-12-14 15:46:03 +0100

    Dynamo integrated

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -9,6 +9,10 @@ import Button from "@material-ui/core/Button";
 import Filter1 from '@material-ui/icons/Filter1'
 import axios from 'axios';
 import './FileUpload.css'
+import Amplify, { Auth, Analytics, Storage, API, graphqlOperation } from 'aws-amplify';
+
+
+
 
 const styles = theme => ({
   root: {
@@ -43,30 +47,16 @@ class TextFields extends Component {
 
   _handleSubmit(e) {
     e.preventDefault();
-    // TODO: do something with -> this.state.file
-    console.log('handle uploading-', this.state.file);
-  }
-
-  _handleImageChange(e) {
-    e.preventDefault();
-
-    let reader = new FileReader();
-    let file = e.target.files[0];
-
-    reader.onloadend = () => {
-      this.setState({
-        file: file,
-        imagePreviewUrl: reader.result
-      });
-    }
-
-    reader.readAsDataURL(file)
+    this.post();
+    console.log('posting');
   }
 
 
   state = {
-    file: '',
-    imagePreviewUrl: ''
+    email:'',
+    firstName:'',
+    middleName:'',
+    surname:''
   };
 
   handleChange = name => event => {
@@ -75,24 +65,42 @@ class TextFields extends Component {
     });
   };
 
+  post = async () => {
+    console.log('calling api');
+    const response = await API.post('preKYCapi', '/items', {
+      body: {
+        email:this.state.email,
+        firstName:this.state.firstName,
+        middleName:this.state.middleName,
+        surname:this.state.surname
+      }
+    });
+    alert(JSON.stringify(response, null, 2));
+  }
+  get = async () => {
+    console.log('calling api');
+    const response = await API.get('preKYCapi', '/items/object/1');
+    alert(JSON.stringify(response, null, 2));
+  }
+  list = async () => {
+    console.log('calling api');
+    const response = await API.get('preKYCapi', '/items/1');
+    alert(JSON.stringify(response, null, 2));
+  }
+  user = async () => {
+    console.log('calling api');
+    const response = Auth.currentAuthenticatedUser();
+    alert(JSON.stringify(response, null, 2));
+  }
+
   render() {
 
-    let {imagePreviewUrl} = this.state;
-    let $imagePreview = null;
-    let $frontPreview = null;
-    let $backPreview = null;
-    let $facePreview = null;
-    if (imagePreviewUrl) {
-      $imagePreview = (<img src={imagePreviewUrl} />);
-      $frontPreview = (<img src={imagePreviewUrl} />);
-      $backPreview = (<img src={imagePreviewUrl} />);
-      $facePreview = (<img src={imagePreviewUrl} />);
-    } else {
-      $imagePreview = (<div className="previewText">Please select an image</div>);
-      $frontPreview = (<div className="previewText">Please select an image</div>);
-      $backPreview = (<div className="previewText">Please select an image</div>);
-      $facePreview = (<div className="previewText">Please select an image</div>);
-    }
+    Auth.currentAuthenticatedUser({
+      bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+  }).then(user => {
+    this.state.email=user.attributes.email;
+  })
+  .catch(err => console.log(err));
 
     const { classes } = this.props;
 
@@ -115,7 +123,7 @@ class TextFields extends Component {
                     label="Given Name"
                     className={classes.textField}
                     value={this.state.firstName}
-                    onChange={this.handleChange("name")}
+                    onChange={this.handleChange("firstName")}
                     margin="normal"
                   />
                   <TextField
@@ -124,7 +132,7 @@ class TextFields extends Component {
                     label="Middle Name"
                     className={classes.textField}
                     value={this.state.middleName}
-                    onChange={this.handleChange("name")}
+                    onChange={this.handleChange("middleName")}
                     margin="normal"
                   />
                   <TextField
@@ -133,159 +141,18 @@ class TextFields extends Component {
                     label="Surname"
                     className={classes.textField}
                     value={this.state.lastName}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Street Address"
-                    id="physical-address"
-                    label="Physical Address"
-                    className={classes.textField}
-                    value={this.state.physicalAddress}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Unit/Apt."
-                    id="continued-address"
-                    label="Unit/Apt."
-                    className={classes.textField}
-                    value={this.state.secondAddress}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="City"
-                    id="city"
-                    label="City"
-                    className={classes.textField}
-                    value={this.state.city}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Country"
-                    id="country"
-                    label="Country"
-                    className={classes.textField}
-                    value={this.state.country}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Region/State"
-                    id="region"
-                    label="Region"
-                    className={classes.textField}
-                    value={this.state.region}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Zip Code"
-                    id="zip-code"
-                    label="Zip Code"
-                    className={classes.textField}
-                    value={this.state.zipCode}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Date of Birth"
-                    id="dob"
-                    label="Date of Birth"
-                    className={classes.textField}
-                    value={this.state.dob}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Occupation"
-                    id="occupation"
-                    label="Occupation"
-                    className={classes.textField}
-                    value={this.state.occupation}
-                    onChange={this.handleChange("name")}
-                    margin="normal"
-                  />
-
-                  <TextField
-                    placeholder="Investment Amount"
-                    id="investment-amount"
-                    label="Investment Amount"
-                    className={classes.textField}
-                    value={this.state.investAmount}
-                    onChange={this.handleChange("name")}
+                    onChange={this.handleChange("surname")}
                     margin="normal"
                   />
 
                 </form>
+                <Button className="submitButton" 
+                  variant="contained"
+                  component="span"
+                  onClick={(e)=>this._handleSubmit(e)}>
+                  SUBMIT
+                </Button>
               </CardContent>
-        <div className="previewComponent">
-        <form onSubmit={(e)=>this._handleSubmit(e)}>
-          <input className="fileInput"
-            id="contained-button-uploadfront" 
-            type="file" 
-            onChange={(e)=>this._handleImageChange(e)} />
-          <label htmlFor="contained-button-uploadfront">
-          <Button className="submitButton" 
-            variant="contained"
-            component="span"
-            onChange={(e)=>this._handleImageChange(e)}>
-            Upload Front ID Image
-            </Button>
-            </label>
-        </form>
-        <div className="imgPreview">
-          {$frontPreview}
-        </div>
-      </div>
-      <div className="previewComponent">
-        <form onSubmit={(e)=>this._handleSubmit(e)}>
-          <input className="fileInput"
-            id="contained-button-uploadback" 
-            type="file" 
-            onChange={(e)=>this._handleImageChange(e)} />
-          <label htmlFor="contained-button-uploadback">
-          <Button className="submitButton" 
-            variant="contained"
-            component="span"
-            onChange={(e)=>this._handleImageChange(e)}>
-            Upload Back ID Image
-            </Button>
-            </label>
-        </form>
-        <div className="imgPreview">
-          {$backPreview}
-        </div>
-      </div>
-      <div className="previewComponent">
-        <form onSubmit={(e)=>this._handleSubmit(e)}>
-          <input className="fileInput"
-            id="contained-button-uploadface" 
-            type="file" 
-            onChange={(e)=>this._handleImageChange(e)} />
-          <label htmlFor="contained-button-uploadface">
-          <Button className="submitButton" 
-            variant="contained"
-            component="span"
-            onChange={(e)=>this._handleImageChange(e)}>
-            Upload Face Image
-            </Button>
-            </label>
-        </form>
-        <div className="imgPreview">
-          {$facePreview}
-        </div>
-      </div>
             </Card>
           </Grid>
           <Grid item xs />
