1afe2334d849a993c08ca79b0e6ce9a4e40f8096
Simon <simone.baroni@looklateral.com>
2018-12-20 15:21:39 +0100

    moved register button in content Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-//import React, { Component } from "react";
+//import React from 'react';
+import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -9,7 +9,7 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-//import Amplify, { Auth, Analytics, Storage, API, graphqlOperation } from 'aws-amplify';
+import Amplify, { Auth, Analytics, Storage, API, graphqlOperation } from 'aws-amplify';
 import Background from '../../shared/images/bg_kyc/14122018-01.JPG';
 //import LogoLL from '../../shared/images/Archivio/kyc_logo_01.png';
 //import LogoLL from '../../shared/images/logo_01.png';
@@ -54,72 +54,121 @@ const styles = theme => ({
     textDecoration: 'none',
   }, 
   register: {
-    color: '#fff',
+    color: '#000',
     fontSize: 15,
     borderStyle: 'solid',
-    borderColor: '#fff',
+    borderColor: '#000',
     borderRadius: 4,
     border: 2,
+    marginTop:40,
   },
 });
 
-function Home(props) {
-  
-  const { classes } = props;
-  let url = '/register'; let label = 'Register';
-    
-    
-    
-  if(this.state.step1 === false) {
-    url = '/register'; label = 'Register';
-    //} else if(props.userState.step1 === true && props.userState.step2 === false) {
-    //  url = '/investor'; label = 'Register'; 
-  } else {
-    url = '/dashboard'; label = 'Dashboard';
-  }
+//function Home(props) {
+class Home extends Component {
 
-  return (
-    <div className={classes.root}>
-    <Grid container spacing={24}>
-    <Grid item xs></Grid>
-    <Grid item xs={6}>
-    <Card className={classes.card}>
+  url = '/register'; 
+  label = 'Register';
+
+  state = {
+    email:'',
+    firstName:'',
+    middleName:'',
+    surname:'',
+    address:'',
+    city:'',
+    zipCode:'',
+    regionState:'',  
+    countryCitizenship:'',
+    countryResidence:'',
+    dateBirth:'',
+    occupation:'',
+    amount:'',
+    step1: '',
+
+    url:'/register',
+    label: 'Register',
+
+    open: false,
+    buttonIsHovered: false,
+  };
+
+  getUser = async () => {
+    const response = await API.get('preKYCapi', '/items/object/' + this.state.email);
     
-    <CardMedia
-    className={classes.imageLL}
-    component='img' 
-    image={require("../../shared/images/logo_01.png")} 
-    title="Look Lateral KYC"
-    />
-    
-    <CardContent>
-    
-    <Typography className={classes.subtitle}>
-    WELCOME TO THE LOOK LATERAL SECURITY TOKEN OFFERING
-    </Typography>
-    
-    <Typography className={classes.title} gutterBottom>
-    KYC FIRST STEP<br />PRE-QUALIFICATION PROCEDURE
-    </Typography>
-    
-    <Typography className={classes.pos}>
-    This procedure is necessary to verify that you are qualified to purchase the Look security token, issued under Reg D and Reg S exemptions.<br /><br />
-    Once you receive our approval you will have access to the full private placement memorandum, the complete white paper and the STO terms and conditions.
-    </Typography>
-    
-    <Link className={classes.registerButton}
-          to={url}>
-      <Button className={classes.register}>{label}</Button>
-    </Link>
-    
-    </CardContent>
-    </Card>
-    </Grid>
-    <Grid item xs></Grid>
-    </Grid>
-    </div>
+    if(response.step1 === false) {
+      this.setState({
+        url: '/register',
+        label: 'Register',
+      });
+
+    } else {
+      this.setState({
+        url: '/dashboard',
+        label: 'Dashboard',
+      });
+    }
+  } 
+  
+
+  render() {
+
+    Auth.currentAuthenticatedUser({
+        bypassCache: false  
+    }).then(user => {
+      
+      if(this.state.email !== user.attributes.email)
+        this.setState({
+          email: user.attributes.email
+        });
+      this.getUser();
+    })
+    .catch(err => console.log(err));
+  
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+      <Grid container spacing={24}>
+      <Grid item xs></Grid>
+      <Grid item xs={6}>
+      <Card className={classes.card}>
+      
+      <CardMedia
+        className={classes.imageLL}
+        component='img' 
+        image={require("../../shared/images/logo_01.png")} 
+        title="Look Lateral KYC"
+      />
+      
+      <CardContent>
+      
+        <Typography className={classes.subtitle}>
+        WELCOME TO THE LOOK LATERAL SECURITY TOKEN OFFERING
+        </Typography>
+        
+        <Typography className={classes.title} gutterBottom>
+        KYC FIRST STEP<br />PRE-QUALIFICATION PROCEDURE
+        </Typography>
+        
+        <Typography className={classes.pos}>
+        This procedure is necessary to verify that you are qualified to purchase the Look security token, issued under Reg D and Reg S exemptions.<br /><br />
+        Once you receive our approval you will have access to the full private placement memorandum, the complete white paper and the STO terms and conditions.
+        </Typography>
+        
+        <Link className={classes.registerButton} to={this.state.url}>
+          <Button className={classes.register}>{this.state.label}</Button>
+        </Link>
+        
+      </CardContent>
+      </Card>
+      </Grid>
+      <Grid item xs></Grid>
+      </Grid>
+      </div>
     );
   }
+}
   
   Home.propTypes = {
     classes: PropTypes.object.isRequired,
