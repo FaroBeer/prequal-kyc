bc78d0a391f2ce395663445c69aa14a51cc84ad8
Simon <simone.baroni@looklateral.com>
2018-12-20 17:16:17 +0100

    added style to Dashboard - WAITING FOR CORRECT PICTURE

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Grid from "@material-ui/core/Grid";
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
 //import axios from 'axios';
 //import './FileUpload.css'
 import { Auth, API } from 'aws-amplify';
@@ -18,14 +19,11 @@ const styles = theme => ({
     backgroundImage: `url(${Background})`,
     marginTop: -150,
   },
-  input: {
-    display: 'none',
-  },
   card: {
     marginTop: 175,
     marginBottom: 40,
     minWidth: 275,
-    minHeight: 650,
+    //minHeight: 650,
     padding: theme.spacing.unit * 2,
     textAlign: "center",
     color: theme.palette.text.secondary
@@ -34,10 +32,38 @@ const styles = theme => ({
     display: "flex",
     flexWrap: "wrap"
   },
-  
   dense: {
     marginTop: 19
-  }
+  },
+  tableHome: {
+    width: '100%',
+    height: '100%',
+    marginBottom: 50,
+  },
+  tdImage: {
+    width: '50%',
+    padding: 5,
+  },
+  tdText: {
+    width: '50%',
+    padding: 10,
+    textAlign: 'center',
+    verticalAlign: 'center',
+  },
+  imageThanks: {
+    width: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  title: {
+    fontSize: 25,
+    color: 'rgb(0,0,256,0.9)',
+    marginBottom: 40,
+  },
+  text: {
+    fontSize: 20,
+    color: 'rgb(0,0,256,0.54)',
+  },
 });
 
 class Dashboard extends Component {
@@ -108,15 +134,30 @@ class Dashboard extends Component {
       <Grid item xs={6}>
       <Card className={classes.card}>
         <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            Dashboard
-          </Typography>
-          <Typography variant="h5" component="h2">
-          Thank you for your application.
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-          We will review your information as soon as possible and we will let you know if you are qualified to proceed to the second step of the KYC.
-          </Typography>
+          
+          
+          <table className={classes.tableHome}>
+            <tr>
+              <td className={classes.tdImage}>
+                <CardMedia
+                  className={classes.imageThanks}
+                  component='img' 
+                  image={require("../../shared/images/18122018-thanks_page02.JPG")} 
+                  title="Look Lateral KYC"
+                />
+              </td>
+              <td className={classes.tdText}>
+                <Typography className={classes.title} gutterBottom>
+                THANK YOU FOR YOUR APPLICATION
+                </Typography>
+                
+                <Typography className={classes.text} gutterBottom>
+                We will review your information as soon as possible and we will let you know if you are qualified to proceed to the second step of the KYC.
+                </Typography>
+              </td>
+            </tr>
+          </table>
+
         </CardContent>
       </Card>
       </Grid>
