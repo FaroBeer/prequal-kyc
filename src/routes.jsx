715b3a66949b9367fc8ec7444107b665de66e628
FaroBeer <calmapazienza@gmail.com>
2018-12-21 15:58:28 +0100

    auth theme work in progress

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,8 +9,17 @@ import Error404 from './components/Error/404';
 import Register from './components/Register';
 //import Investor from './components/Investor';
 import Dashboard from './components/Dashboard';
+import { Authenticator } from 'aws-amplify-react';
+
+import { AmplifyTheme } from 'aws-amplify-react';
+
+const MySectionHeader = Object.assign({}, AmplifyTheme.sectionHeader, { background: 'orange' });
+const MySectionBody = Object.assign({}, AmplifyTheme.sectionBody, {background: 'purple'})
+//const MyContainer = Object.assign({}, AmplifyTheme.container, {backgroundColor: 'black'})
+const MyTheme = Object.assign({}, AmplifyTheme, { sectionHeader: MySectionHeader , sectionBody: MySectionBody, Container: MyContainer});
 
 const AppRoutes = () => (
+  <Authenticator theme={MyTheme}>
   <App>
     <Switch>
       <Route path="/" component={Home} exact />
@@ -19,6 +28,7 @@ const AppRoutes = () => (
       <Route component={Error404} />
     </Switch>
   </App>
+  </Authenticator>
 );
 
 export default AppRoutes;
